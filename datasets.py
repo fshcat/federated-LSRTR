84bffd730f32d8755a470636a82ef607c62bc00f
fshcat <jhoyos314@gmail.com>
2023-08-02 22:09:26 -0400

    compare site sizes

--- datasets.py
+++ datasets.py
@@ -2,19 +2,23 @@ from lsr_tensor import *
 import torch.nn.functional as f
 import torch
 
+
+@torch.no_grad()
+def get_synth_tensor(shape, ranks, separation_rank):
+    true_lsr = LSR_tensor_dot(shape, ranks, separation_rank)
+    f.normalize(true_lsr.core_tensor, p=2, dim=0, out=true_lsr.core_tensor)
+    true_lsr.core_tensor *= (5 / torch.sqrt(torch.sqrt(torch.prod(torch.tensor(ranks)))))
+
+    return true_lsr
+
 # Create synthetic data using the given tensor as the underlying parameters of the distribution
 @torch.no_grad()
-def synthesize_data(shape, ranks, separation_rank, train_num, val_num):
+def synthesize_data(true_lsr, train_num, val_num):
     x_stdev = 1
     y_stdev = 0.05
 
-    with torch.no_grad():
-        true_lsr = LSR_tensor_dot(shape, ranks, separation_rank)
-        f.normalize(true_lsr.core_tensor, p=2, dim=0, out=true_lsr.core_tensor)
-        true_lsr.core_tensor *= (5 / torch.sqrt(torch.sqrt(torch.prod(torch.tensor(ranks)))))
-
-    x_train = torch.randn((train_num, *shape)) * x_stdev
-    x_val = torch.randn((val_num, *shape)) * x_stdev
+    x_train = torch.randn((train_num, *true_lsr.shape)) * x_stdev
+    x_val = torch.randn((val_num, *true_lsr.shape)) * x_stdev
 
     y_train = true_lsr.forward(x_train) + torch.randn_like(true_lsr.forward(x_train)) * y_stdev
     y_val = true_lsr.forward(x_val) + torch.randn_like(true_lsr.forward(x_val)) * y_stdev
